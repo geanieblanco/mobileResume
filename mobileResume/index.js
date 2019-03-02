e7b2140d176f65c3d88b27419808f61650b4f392
Geanie Blanco <geanieblanco@gmail.com>
2019-03-02 09:28:16 -0500

    major UI updates

--- mobileResume/index.js
+++ mobileResume/index.js
@@ -5,6 +5,11 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+// // import Header from './src/components/header';
+// import Resume from './src/screens/resume';
+// import About from './src/screens/resume';
+
+
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
